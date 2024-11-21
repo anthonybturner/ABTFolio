c4926cb2c55949949990c2bb2216e5d9d46cf688
anthony <anthonybturner@icloud.com>
2024-11-21 07:28:49 -0500

    simple yaml

---
+++ app/static/precache-manifest.c236e36b99c5e2b8fdbcf3e0171da78e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06360083d540b7545c0ca3080331cc97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4161402346a10cdea2d4",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "388f518fc63220ec9b06",
+    "url": "/static/js/2.41b6f003.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.41b6f003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a6fdcfc9f014649d93",
+    "url": "/static/js/3.60db2ff6.chunk.js"
+  },
+  {
+    "revision": "4161402346a10cdea2d4",
+    "url": "/static/js/main.d62ff4dd.chunk.js"
+  },
+  {
+    "revision": "11ef0910b93614659c57",
+    "url": "/static/js/runtime-main.9dbde3e4.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
